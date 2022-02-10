5fe7b972c1768a325488a00423d0e579bb85b5d6
Cody.Vernon <cody.vernon@wsu.edu>
2022-02-10 14:07:26 -0800

    Set up classes

---
+++ ranking.py
@@ -0,0 +1,24 @@
+import sys
+
+class TF_IDF(object):
+
+    def __init__(self, dataFile):
+        pass
+
+    def tf_idf(self, Q, k):
+        pass
+
+    def relevance(self, d, Q):
+        pass
+
+    def tf(self, d, t):
+        pass
+
+
+class BM_25(object):
+
+    def __init__(self, dataFile):
+        pass
+
+    def bm25(self, Q, k):
+        pass
